626e1b64f17abbbb2c07e9daf5542541134f5f6a
dhanyashettigar <dhanyashettigar19@gmail.com>
2026-06-17 16:28:20 +0530

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -42,7 +42,7 @@ if load_btn:
         with st.spinner("Loading embeddings..."):
             embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-large-en-v1.5")
             vectorstore = FAISS.load_local(
-                FAISS_INDEX_PATH, embeddings, allow_dangerous_deserialization=True
+                MEDICAL_FAISS_INDEX_PATH, embeddings, allow_dangerous_deserialization=True
             )
             retriever = vectorstore.as_retriever(search_kwargs={"k": 6})
 
